a064e227a4fd3fee235a2c5de93aa1bb0eeba225
Tadea Simunovic <tsimunoviczg@gmail.com>
2018-09-10 00:39:02 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
       erb :super_hero
     end
 
-    post '/team' do
+    post '/teams' do
     @team = Team.new(params[:team])
     params[:team][:members].each do |info|
       SuperHero.new(info)
