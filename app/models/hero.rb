5220710113407baa44d2d35651d92fab36915f41
Tadea Simunovic <tsimunoviczg@gmail.com>
2018-09-10 00:12:33 +0000

    automatically backed up by learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,4 +1,10 @@
 class Hero
+  attr_reader :name, :power, :biography
 
-
+  HEROES = []
+  def initialize(params)
+    @name = params[name]
+    @power = params[power]
+    @biography = params[biography]
+    HEROES << self
 end
